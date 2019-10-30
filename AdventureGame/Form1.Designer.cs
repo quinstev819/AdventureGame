aee02fccedec2fc85c93d7b1326bbe893660b342
quinstev819 <quinstev819@ed.amdsb.ca>
2019-10-30 13:32:18 -0400

    October 30th, 2019

--- AdventureGame/Form1.Designer.cs
+++ AdventureGame/Form1.Designer.cs
@@ -28,20 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.redImage = new System.Windows.Forms.PictureBox();
+            this.blueImage = new System.Windows.Forms.PictureBox();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.yellowImage = new System.Windows.Forms.PictureBox();
+            this.yellowLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.redImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowImage)).BeginInit();
             this.SuspendLayout();
+            // 
+            // redImage
+            // 
+            this.redImage.Image = global::AdventureGame.Properties.Resources.red_50x50;
+            this.redImage.Location = new System.Drawing.Point(24, 303);
+            this.redImage.Margin = new System.Windows.Forms.Padding(2);
+            this.redImage.Name = "redImage";
+            this.redImage.Size = new System.Drawing.Size(39, 32);
+            this.redImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.redImage.TabIndex = 0;
+            this.redImage.TabStop = false;
+            // 
+            // blueImage
+            // 
+            this.blueImage.Image = global::AdventureGame.Properties.Resources.blue_50x50;
+            this.blueImage.Location = new System.Drawing.Point(24, 339);
+            this.blueImage.Margin = new System.Windows.Forms.Padding(2);
+            this.blueImage.Name = "blueImage";
+            this.blueImage.Size = new System.Drawing.Size(39, 32);
+            this.blueImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.blueImage.TabIndex = 1;
+            this.blueImage.TabStop = false;
+            // 
+            // redLabel
+            // 
+            this.redLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLabel.ForeColor = System.Drawing.Color.White;
+            this.redLabel.Location = new System.Drawing.Point(67, 313);
+            this.redLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(257, 23);
+            this.redLabel.TabIndex = 2;
+            this.redLabel.Text = "Right";
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLabel.ForeColor = System.Drawing.Color.White;
+            this.blueLabel.Location = new System.Drawing.Point(67, 349);
+            this.blueLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(257, 23);
+            this.blueLabel.TabIndex = 3;
+            this.blueLabel.Text = "Left";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(24, 28);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(339, 55);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.Text = "You find yourself in a room. There are three doors. Which door would you like to " +
+    "try first?";
+            // 
+            // yellowImage
+            // 
+            this.yellowImage.Image = global::AdventureGame.Properties.Resources.yellow_50x50;
+            this.yellowImage.Location = new System.Drawing.Point(24, 375);
+            this.yellowImage.Margin = new System.Windows.Forms.Padding(2);
+            this.yellowImage.Name = "yellowImage";
+            this.yellowImage.Size = new System.Drawing.Size(39, 32);
+            this.yellowImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.yellowImage.TabIndex = 5;
+            this.yellowImage.TabStop = false;
+            // 
+            // yellowLabel
+            // 
+            this.yellowLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yellowLabel.ForeColor = System.Drawing.Color.White;
+            this.yellowLabel.Location = new System.Drawing.Point(67, 385);
+            this.yellowLabel.Name = "yellowLabel";
+            this.yellowLabel.Size = new System.Drawing.Size(100, 23);
+            this.yellowLabel.TabIndex = 6;
+            this.yellowLabel.Text = "Straight";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(386, 422);
+            this.Controls.Add(this.yellowLabel);
+            this.Controls.Add(this.yellowImage);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.blueImage);
+            this.Controls.Add(this.redImage);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Lost in a building";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.redImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowImage)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox redImage;
+        private System.Windows.Forms.PictureBox blueImage;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox yellowImage;
+        private System.Windows.Forms.Label yellowLabel;
     }
 }
 
--- AdventureGame/Form1.cs
+++ AdventureGame/Form1.cs
@@ -7,14 +7,201 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace AdventureGame
 {
     public partial class Form1 : Form
     {
+        //tracks what part of the game the user is at
+        int scene = 0;
+        int randNum;
+        int randNumber = 0;
+        int rightDoor = 0;
+        int leftDoor = 0;
+        int straightDoor = 0;
+
+        //random number generators
+        Random randGen = new Random();
         public Form1()
         {
             InitializeComponent();
+
+            //display initial message and options
+            outputLabel.Text = "You find yourself in a room. There are three doors. Which door would you like to try first?";
+            redLabel.Text = "Right";
+            blueLabel.Text = "Left";
+            yellowLabel.Visible = true;
+            yellowImage.Visible = true;
+            yellowLabel.Text = "Straight";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //check to see what button has been pressed and advance
+            //to the next appropriate scene
+            randNum = randGen.Next(1, 101);
+            if (randNum < 33)
+            {
+                rightDoor = 1;
+            }
+            else if ((33 < randNum) && (randNum < 66))
+            {
+                leftDoor = 1;
+            }
+            else if (randNum > 66)
+            {
+                straightDoor = 1;
+            }
+            randNumber = randGen.Next(1, 101);
+            if (e.KeyCode == Keys.M)
+            {
+                if (scene == 0)
+                {
+                    if (rightDoor == 1)
+                    {
+                        scene = 2;
+                    }
+                    else
+                    {
+                        scene = 1;
+                    }
+                }
+            }
+            if (e.KeyCode == Keys.B)
+            {
+
+            }
+            if (e.KeyCode == Keys.N)
+            {
+
+            }
+            /// Display text and game options to screen based on current scene
+            switch (scene)
+            {
+                case 0:
+                    outputLabel.Text = "You find yourself in a room. There are three doors. Which door would you like to try first?";
+                    redLabel.Text = "Right";
+                    blueLabel.Text = "Left";
+                    yellowLabel.Visible = true;
+                    yellowImage.Visible = true;
+                    yellowLabel.Text = "Straight";
+                    break;
+                case 1:
+                    outputLabel.Text = "The door is locked. Try again.";
+                    redLabel.Text = "Right";
+                    blueLabel.Text = "Left";
+                    yellowLabel.Visible = true;
+                    yellowImage.Visible = true;
+                    yellowLabel.Text = "Straight";
+                    break;
+                case 2:
+                    outputLabel.Text = "The door creaks open. You find yourself in a hallway. Turn left or right?";
+                    redLabel.Text = "Right";
+                    blueLabel.Text = "Left";
+                    yellowImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    break;
+                case 3:
+                    outputLabel.Text = "You find a flashlight. Turn it on?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    break;
+                case 4:
+                    outputLabel.Text = "You see somebody down the hall. Call to them?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    break;
+                case 5:
+                    outputLabel.Text = "They ask you how you got there. Do you tell them you don't know or make up an answer?";
+                    redLabel.Text = "You don't know";
+                    blueLabel.Text = "Make up an answer";
+                    yellowImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    break;
+                case 6:
+                    outputLabel.Text = "They seem skeptical of your answer but believe you. The ask you if you need help.";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    break;
+                case 7:
+                    outputLabel.Text = "They ask you what you are doing here. Do you tell them you don't know or do you try to learn about where you are.";
+                    redLabel.Text = "You don't know";
+                    blueLabel.Text = "Try to learn about where you are";
+                    yellowImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    break;
+                case 8:
+                    outputLabel.Text = "They leave you be. Do you continue down the hall on the right or left?";
+                    redLabel.Text = "Right";
+                    blueLabel.Text = "Left";
+                    yellowImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    break;
+                case 9:
+                    outputLabel.Text = "They leave you be. Do you try to find your way out, or try to figure out where you are?";
+                    redLabel.Text = "Try to find your way out";
+                    blueLabel.Text = "Try to figure out where you are";
+                    yellowImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    break;
+                case 10:
+                    outputLabel.Text = "They ask you if you'd like directions to get out. Do you say yes or no.";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    break;
+                case 11:
+                    outputLabel.Text = "The flashlight turned on revealing a payphone at the end of the hall. Call for help?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    break;
+                case 12:
+                    outputLabel.Text = "You try to find your way around, but you get lost in the building. You never find your way out. Do you want to play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    break;
+                case 13:
+                    outputLabel.Text = "Thank you for playing!";
+                    redLabel.Visible = false;
+                    redImage.Visible = false;
+                    blueLabel.Visible = false;
+                    blueImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    yellowImage.Visible = false;
+                    Thread.Sleep(2000);
+                    this.Close();
+                    break;
+                case 14:
+                    outputLabel.Text = "You find the exit! You get out of the building and live the rest of your life. Do you want to play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    break;
+                case 15:
+                    outputLabel.Text = "Thank you for playing!";
+                    redLabel.Visible = false;
+                    redImage.Visible = false;
+                    blueLabel.Visible = false;
+                    blueImage.Visible = false;
+                    yellowLabel.Visible = false;
+                    yellowImage.Visible = false;
+                    Thread.Sleep(2000);
+                    this.Close();
+                    break;
+            }
         }
     }
 }
